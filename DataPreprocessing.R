2590fcc154b2b776aae7fd6efd4d87068f23e4fe
Ljfernando <Ljfernando@dons.usfca.edu>
2016-10-31 01:11:00 -0700

    Committing changes to datapreprocessing file

--- DataPreprocessing.R
+++ DataPreprocessing.R
@@ -162,7 +162,6 @@ SF_trips <- trip_data[trip_data$Start.Terminal %in% sf_IDs,] # %in% checks if va
 
 curr_time = as.POSIXlt(SF_trips$Start.Date[1], format = "%m/%d/%Y %H:%M");
 
-
 index <- 1:nrow(SF_trips)
 j <- 1
 
@@ -174,13 +173,20 @@ day_total <- rep(0, 400)
 sub_total <- rep(0,400)
 cust_total <- rep(0,400)
 date <- rep(0, 400)
+duration_total <- rep(0, 400)
+sub_dur_total <- rep(0, 400)
+cust_dur_total <- rep(0, 400)
+
 total = 0
 num_sub = 0
 num_cust = 0
+dur_total = 0
+sub_dur = 0
+cust_dur = 0
 for(i in index){
   
   next_row_time <- as.POSIXlt(SF_trips$Start.Date[i], format = "%m/%d/%Y %H:%M")
-  if(next_row_time$yday != curr_time$yday | next_row_time$year != curr_time$year){
+  if(next_row_time$yday != curr_time$yday | next_row_time$year != curr_time$year | i == nrow(SF_trips)){
     # Storing data at index j to new vectors
     day_total[j] <- total
     day[j] <- curr_time$mday
@@ -189,6 +195,9 @@ for(i in index){
     year[j] <- curr_time$year - 100
     sub_total[j] <- num_sub
     cust_total[j] <- num_cust
+    sub_dur_total[j] <- sub_dur
+    cust_dur_total[j] <- cust_dur
+    duration_total[j] <- dur_total
     date[j] <- as.Date(SF_trips$Start.Date[i], format = "%m/%d/%Y")
     
     j <- j + 1
@@ -197,15 +206,20 @@ for(i in index){
     total <- 0
     curr_time <- next_row_time
     num_cust <- 0
-    num_sub <-0
+    num_sub <- 0
+    sub_dur <- 0
+    cust_dur <- 0
   }
   else{
     total <- total + 1
+    dur_total <- dur_total + SF_trips$Duration[i]
     if(SF_trips$Subscriber.Type[i] == "Subscriber"){
       num_sub <- num_sub + 1
+      sub_dur <- sub_dur + SF_trips$Duration[i]
     }
     else{
       num_cust <- num_cust +1
+      cust_dur <- cust_dur + SF_trips$Duration[i]
     }
   }
 }
@@ -213,7 +227,7 @@ for(i in index){
 #date is in numeric form with origin 1970-01-01
 #converting it to readable format
 PDT <- as.Date(date, origin = "1970-01-01")
-SF_totals <- data.frame(PDT, day_total, sub_total, cust_total, day, weekday, month, year)
+SF_totals <- data.frame(PDT, day_total, sub_total, cust_total, duration_total, sub_dur_total, cust_dur_total, day, weekday, month, year)
 
 is_weekend = ifelse(SF_totals$weekday == 0 | SF_totals$weekday == 6, 1, 0)
 SF_totals <- data.frame(SF_totals, is_weekend)
@@ -229,11 +243,24 @@ SF_weather_data$PDT <- as.Date(SF_weather_data$PDT, format = "%m/%d/%Y")
 #Mergeing on dates
 SF_daily_bikeshare <- merge(SF_totals, SF_weather_data, by = "PDT")
 
+rm(curr_time,cust_dur, cust_dur_total,cust_total, date, day, day_total,
+   dur_total, duration_total, holidays, i, index, is_weekend, j ,month, 
+   next_row_time, num_cust, num_sub, PDT, sf_IDs, sub_dur, sub_dur_total, 
+   sub_total, total, weekday, year)
 #~~~~~~~~~~~~~~~~~~~~DATA EXPLORATION~~~~~~~~~~~~~~~~~~
 
-attach(SF_totals)
-par(mfrow = c(1,2))
-plot(SF_totals$weekday, sub_total, xlab = "Day of the week", ylab = "Number of rentals", main = "Subscribers", col = "skyblue")
-plot(SF_totals$weekday, cust_total, xlab = "Day of the week", ylab = "Number of rentals", main = "Customers", col = "slateblue4")
+attach(SF_daily_bikeshare)
+par(mfrow = c(2,2))
+plot(weekday, sub_total, xlab = "Day of the week", ylab = "Number of rentals", main = "Subscribers", col = "skyblue")
+plot(weekday, cust_total, xlab = "Day of the week", ylab = "Number of rentals", main = "Customers", col = "slateblue4")
+plot(PDT, sub_total, col = "orange", xlab = "Month", ylab = "Number of rentals", main = "Subscribers")
+plot(PDT, cust_total, col = "darkred", xlab = "Month", ylab = "Number of rentals", main = "Customers")
+
 dev.off()
 
+par(mfrow = c(2,1))
+plot(PDT, day_total, type = 'line', col = "forestgreen", xlab = "Month", ylab = "Number of rentals", main = "Rentals each month")
+plot(PDT, Mean.TemperatureF, type = 'line', col = "red", xlab = "Month", ylab = "Temperature in F", main = "Temperature each month ")
+lines(PDT, Max.TemperatureF, col = "orange")
+lines(PDT, Min.TemperatureF, col = "lightblue")
+dev.off()
