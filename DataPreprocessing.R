0da76c749a10e2f2e3bb23c516df3d6b26ef30ad
Ljfernando <Ljfernando@dons.usfca.edu>
2016-10-30 23:31:55 -0700

    committing datapreprocessing file

--- DataPreprocessing.R
+++ DataPreprocessing.R
@@ -14,6 +14,7 @@ status_data = na.omit(status_data)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Calculating distance of each trip
+install.packages("geosphere")
 library(geosphere)
 index <- 1:dim(trip_data)[1]
 coordinates <- matrix(nrow = dim(trip_data)[1], ncol = 4)
@@ -39,12 +40,10 @@ trip_data <- data.frame(trip_data, distance)
 install.packages("measurements")
 library(measurements)
 ?conv_unit
-time_diff <- as.numeric(difftime(as.POSIXlt(trip_data$End.Date, 
-             format = "%m/%d/%Y %H:%M"), as.POSIXlt(trip_data$Start.Date,
-             format = "%m/%d/%Y %H:%M"), units = "hours"))
+
 dist_miles <- conv_unit(trip_data$distance, "m", "mi")
-avg_speed <- dist_miles/time_diff
-trip_data <- data.frame(trip_data, avg_speed)
+avg_speed <- dist_miles/(trip_data$Duration/60)
+trip_data <- data.frame(trip_data, dist_miles, avg_speed)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Calculating total number of bikes checked each day
@@ -151,106 +150,90 @@ for(i in index){
 }
 total_each_hour <- data.frame(station_id, hour_total, hour, day, month)
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Calculating hourly totals of San Francisco stations
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Calculating total trips each day in SF
+
+# Grabbing SF station IDs
 sf_IDs <- station_data[station_data$landmark == "San Francisco",]
 sf_IDs <- sf_IDs$station_id
 
-# Creating subset of San Francisco stations
-SF_status_data <- status_data[status_data$station_id == sf_IDs,]
+SF_trips <- trip_data[trip_data$Start.Terminal %in% sf_IDs,] # %in% checks if values in list
 
-curr_time = as.POSIXlt(SF_status_data$time[1], format = "%Y-%m-%d %H:%M:%S");
-curr_available = SF_status_data$bikes_available[1]
-curr_id = SF_status_data$station_id[1]
-total = 0
+curr_time = as.POSIXlt(SF_trips$Start.Date[1], format = "%m/%d/%Y %H:%M");
 
-index <- 1:dim(SF_status_data)[1]
+
+index <- 1:nrow(SF_trips)
 j <- 1
 
-station_id <- rep(0, dim(SF_status_data)[1]/24)
-day <- rep(0, dim(SF_status_data)[1]/24)
-month <- rep(0, dim(SF_status_data)[1]/24)
-hour <- rep(0, dim(SF_status_data)[1]/24)
-hour_total <- rep(0, dim(SF_status_data)[1]/24)
-for(i in index){
-  
-  next_row_time <- as.POSIXlt(SF_status_data[i,4], format = "%Y-%m-%d %H:%M:%S")
-  if(curr_id != SF_status_data$station_id[i] | next_row_time$min < curr_time$min){
-    # Storing data at index j to new vectors
-    station_id[j] <- curr_id
-    day[j] <- curr_time$wday
-    month[j] <- curr_time$mon
-    hour[j] <- curr_time$hour
-    hour_total[j] <- total
-    
-    j <- j + 1
-    # Updating variables
-    total <- 0
-    curr_available <- SF_status_data$bikes_available[i]
-    curr_id <- SF_status_data$station_id[i]
-  }
-  curr_time <- next_row_time
-  # If someone checks out a bike
-  if(curr_available > SF_status_data$bikes_available[i]){
-    
-    total = total + (curr_available - SF_status_data$bikes_available[i])
-    curr_available = SF_status_data$bikes_available[i]
-  }
-  
-  # When someone checks in a bike, total is not effected
-  else if(curr_available < SF_status_data$bikes_available[i]){
-    
-    curr_available = SF_status_data$bikes_available[i]
-  }
-}
-SF_total_each_hour <- data.frame(station_id, hour_total, hour, day, month)
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Calculating total each day in SF
-curr_time = as.POSIXlt(SF_status_data$time[1], format = "%Y-%m-%d %H:%M:%S");
-curr_available = SF_status_data$bikes_available[1]
-curr_id = SF_status_data$station_id[1]
+day <- rep(0, 400)
+weekday <- rep(0, 400)
+month <- rep(0, 400)
+year <- rep(0, 400)
+day_total <- rep(0, 400)
+sub_total <- rep(0,400)
+cust_total <- rep(0,400)
+date <- rep(0, 400)
 total = 0
-
-index <- 1:dim(SF_status_data)[1]
-j <- 1
-
-station_id <- rep(0, dim(SF_status_data)[1]/30)
-day <- rep(0, dim(SF_status_data)[1]/30)
-weekday <- rep(0, dim(SF_status_data)[1]/30)
-month <- rep(0, dim(SF_status_data)[1]/30)
-day_total <- rep(0, dim(SF_status_data)[1]/30)
+num_sub = 0
+num_cust = 0
 for(i in index){
   
-  next_row_time <- as.POSIXlt(SF_status_data[i,4], format = "%Y-%m-%d %H:%M:%S")
-  if(curr_id != SF_status_data$station_id[i] | next_row_time$yday > curr_time$yday | next_row_time$year > curr_time$year){
+  next_row_time <- as.POSIXlt(SF_trips$Start.Date[i], format = "%m/%d/%Y %H:%M")
+  if(next_row_time$yday != curr_time$yday | next_row_time$year != curr_time$year){
     # Storing data at index j to new vectors
-    station_id[j] <- curr_id
     day_total[j] <- total
     day[j] <- curr_time$mday
     weekday[j] <- curr_time$wday
     month[j] <- curr_time$mon
+    year[j] <- curr_time$year - 100
+    sub_total[j] <- num_sub
+    cust_total[j] <- num_cust
+    date[j] <- as.Date(SF_trips$Start.Date[i], format = "%m/%d/%Y")
     
     j <- j + 1
+    
     # Updating variables
     total <- 0
-    curr_available <- SF_status_data$bikes_available[i]
     curr_time <- next_row_time
-    curr_id <- SF_status_data$station_id[i]
+    num_cust <- 0
+    num_sub <-0
   }
-  
-  # If someone checks out a bike
-  if(curr_available > SF_status_data$bikes_available[i]){
-    
-    total = total + (curr_available - SF_status_data$bikes_available[i])
-    curr_available = SF_status_data$bikes_available[i]
-  }
-  
-  # When someone checks in a bike, total is not effected
-  else if(curr_available < SF_status_data$bikes_available[i]){
-    
-    curr_available = SF_status_data$bikes_available[i]
+  else{
+    total <- total + 1
+    if(SF_trips$Subscriber.Type[i] == "Subscriber"){
+      num_sub <- num_sub + 1
+    }
+    else{
+      num_cust <- num_cust +1
+    }
   }
 }
-SF_total_each_day <- data.frame(station_id,day_total, day, weekday, month)
+
+#date is in numeric form with origin 1970-01-01
+#converting it to readable format
+PDT <- as.Date(date, origin = "1970-01-01")
+SF_totals <- data.frame(PDT, day_total, sub_total, cust_total, day, weekday, month, year)
+
+is_weekend = ifelse(SF_totals$weekday == 0 | SF_totals$weekday == 6, 1, 0)
+SF_totals <- data.frame(SF_totals, is_weekend)
+
+#~~~~~~~~~~~~~~~
+# Mergeing weather and SF_totals
+
+SF_weather_data <- weather_data[weather_data$Zip == 94107,]
+#Changing format to match in both datasets
+SF_weather_data$PDT <- as.Date(SF_weather_data$PDT, format = "%m/%d/%Y")
+?merge
+
+#Mergeing on dates
+SF_daily_bikeshare <- merge(SF_totals, SF_weather_data, by = "PDT")
+
+#~~~~~~~~~~~~~~~~~~~~DATA EXPLORATION~~~~~~~~~~~~~~~~~~
+
+attach(SF_totals)
+par(mfrow = c(1,2))
+plot(SF_totals$weekday, sub_total, xlab = "Day of the week", ylab = "Number of rentals", main = "Subscribers", col = "skyblue")
+plot(SF_totals$weekday, cust_total, xlab = "Day of the week", ylab = "Number of rentals", main = "Customers", col = "slateblue4")
+dev.off()
 
